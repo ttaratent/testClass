2c0352bbe5cf204fc329d80e8202ec3d929fcf06
ttaratent <sqb5290@gmail.com>
2017-09-26 16:21:20 +0800

    动态代理

---
+++ src/com/sqb/proxy/dynamicproxy/App.java
@@ -0,0 +1,21 @@
+package com.sqb.proxy.dynamicproxy;
+
+import com.sqb.proxy.staticproxy.IUserDao;
+import com.sqb.proxy.staticproxy.UserDao;
+
+public class App {
+    public static void main(String[] args) {
+        // 目标对象
+        IUserDao target = new UserDao();
+        // 【原始的类型 class】
+        System.out.println(target.getClass());
+        
+        // 给目标对象，创建代理对象
+        IUserDao proxy = (IUserDao) new ProxyFactory(target).getProxyInstance();
+        // class $Proxy0 内存中动态生成的代理对象
+        System.out.println(proxy.getClass());
+        
+        // 执行方法   【代理对象】
+        proxy.save();
+    }
+}
