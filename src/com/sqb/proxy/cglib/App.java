74d6e95a0a1a24b3bddefd8bcdef0cc136580a4b
ttaratent <sqb5290@gmail.com>
2017-09-26 16:22:14 +0800

    spring集成cglib子类代理

---
+++ src/com/sqb/proxy/cglib/App.java
@@ -0,0 +1,15 @@
+package com.sqb.proxy.cglib;
+
+public class App {
+//    @Test
+//    public void test(){
+//        //目标对象
+//        UserDao target = new UserDao();
+//
+//        //代理对象
+//        UserDao proxy = (UserDao)new ProxyFactory(target).getProxyInstance();
+//
+//        //执行代理对象的方法
+//        proxy.save();
+//    }
+}
---
+++ src/com/sqb/proxy/cglib/UserDao.java
@@ -0,0 +1,7 @@
+package com.sqb.proxy.cglib;
+
+public class UserDao {
+    public void save(){
+        System.out.println("----已经保存数据！----");
+    }
+}
