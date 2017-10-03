73e68a57b19836eed66c6773cc8d6dcfad8de7a5
ttaratent <sqb5290@gmail.com>
2017-10-03 15:30:42 +0800

    原型模式

---
+++ src/com/sqb/designpattern/prototype/Prototype.java
@@ -0,0 +1,11 @@
+package com.sqb.designpattern.prototype;
+
+public class Prototype implements Cloneable {
+    
+    public Object clone() throws CloneNotSupportedException {
+        
+        Prototype proto = (Prototype) super.clone();
+        
+        return proto;
+    }
+}
---
+++ src/com/sqb/designpattern/prototype/SerializableObject.java
@@ -0,0 +1,12 @@
+package com.sqb.designpattern.prototype;
+
+import java.io.Serializable;
+
+public class SerializableObject implements Serializable{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+}
