ae6f454525ceb51b9f3d37b69dcbe7dbef02c1c9
ttaratent <sqb5290@gmail.com>
2017-10-13 23:06:37 +0800

    模板方法模式

---
+++ src/com/sqb/designpattern/templatemethod/StrategyTest.java
@@ -0,0 +1,10 @@
+package com.sqb.designpattern.templatemethod;
+
+public class StrategyTest {
+    public static void main(String[] args) {
+        String exp = "8+8";
+        AbstractCalculator cal = new Plus();
+        int result = cal.calculate(exp, "\\+");
+        System.out.println(result);
+    }
+}
