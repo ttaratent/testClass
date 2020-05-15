b1361ad41a07efb87ef05bb95558daa6a1e412f5
GameFF <sqb5290@gmail.com>
2020-05-15 18:05:05 +0800

    双指针

--- src/com/sqb/algorithm/learn/NumSeriesMaxArea.java
+++ src/com/sqb/algorithm/learn/NumSeriesMaxArea.java
@@ -5,8 +5,12 @@ package com.sqb.algorithm.learn;
  */
 public class NumSeriesMaxArea {
 	
+	public static void main(String[] args) {
+		maxArea(new int[] {1,8,6,2,5,4,8,3,7});
+	}
+	
 	// 1. 两个循环 遍历 查询最大面积
-	public int maxArea(int[] height) {
+	public static int maxArea1(int[] height) {
 		int len = height.length;
 		int max = 0;
 		for(int i = 0; i < len; i++) {
@@ -25,12 +29,25 @@ public class NumSeriesMaxArea {
     }
 	
 	// 头尾双指针 
-	public int maxArea1(int[] height) {
+	public static int maxArea(int[] height) {
 		int i = 0;
 		int j = height.length - 1;
 		int max = 0;
-		for(;i >= j;) {
-			
+		for(;i < j;) {
+			int yi = height[i];
+			int yj = height[j];
+			int h = j - i;
+			int min = Math.min(yi, yj);
+			int area = h * min;
+			if(max < area) {
+				max = area;
+			}
+			if(yi <= yj) {
+				i++;
+			} else {
+				j--;
+			}
 		}
+		return max;
 	}
 }
